d7bb5ed0ed9ab27d6f528f92d104e7511bba7d7a
qdvu124 <vudinhquan124@gmail.com>
2016-09-20 19:27:54 +0100

    Allowed cross domain service in header

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ var port = process.env.PORT || 2302;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
+router.all('/', function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+})
 // test route to make sure everything is working (accessed at GET http://localhost:2302/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
