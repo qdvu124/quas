a8e858d3783864021957c30c7cde9385f2447d0c
Phi Khanh An <blacksteed232@Phifish.local>
2016-09-20 14:54:34 -0400

    Test Bear Model

--- server.js
+++ server.js
@@ -4,9 +4,10 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');        // call express
+var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var Bear = require('./app/models/bear');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,14 +16,45 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 2302;        // set our port
 
+var mongoose = require('mongoose');
+// mongoose.connect('mongodb://quaan24:quaan24@ds033126.mlab.com:33126/quas-test'); // connect to our databas
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:2302/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+// middleware to use for all requests
+router.use(function (req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
 });
+
+// test route to make sure everything is working (accessed at GET http://localhost:2302/api)
+router.get('/', function (req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function (req, res) {
+
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function (err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+
+    });
 
 // more routes for our API will happen here
 
@@ -34,3 +66,10 @@ app.use('/api', router);
 // =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+// {
+//     name: "asd",
+//     edition: "1.2",
+//     author: "123",
+//     publisher: "123",
+// }
