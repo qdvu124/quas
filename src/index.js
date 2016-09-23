b14d2119a5621ed8686cea8cbfb89003d9875add
qdvu124 <vudinhquan124@gmail.com>
2016-09-23 14:45:22 +0100

    Added very basic tab views for listing and posting books

--- src/index.js
+++ src/index.js
@@ -2,27 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
-import BookForm from './components/book_form';
-import BookList from './components/book_list';
+import TabBody from './components/tab_body'
 
 class App extends React.Component {
     constructor(props){
         super(props);
-
-        this.state = { };
+        this.state = { currentTab: 'list' };
     }
 
     render() {
-
-
-        return (
-            <div> 
-                <SearchBar />
-                <br />
-                <BookForm />
-                <br />
-                <BookList />
-            </div>
+         return(
+              <div> 
+                <ul > 
+                    <button onClick={() => this.setState({ currentTab: 'list'})}>Menu 1</button>
+                    <button onClick={() => this.setState({ currentTab: 'post'})}>Menu 2</button>
+                </ul>
+                <TabBody currentTab={this.state.currentTab}/>
+              </div>
         );
     }
 }
